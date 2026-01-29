23b306027853bb922f6a9be825110c18e5273ca7
Pawel Kotzbach <pawkotz@gmail.com>
2026-01-29 19:25:36 +0100

    add decent iris

--- Examples/iris.cpp
+++ Examples/iris.cpp
@@ -5,6 +5,7 @@
 #include "loss.hpp"
 #include <iostream>
 #include <map>
+#include <random>
 
 #include <chrono>
 #include <ctime>
@@ -13,91 +14,105 @@ int main()
 {
     std::time_t currentTime = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
     std::cout << "Current time: " << std::ctime(&currentTime);
+    
     // make df
     std::vector<std::vector<std::string>> csv = loadCSV("Examples/Datasets/Iris.csv");
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::shuffle(csv.begin(), csv.end(), gen);
     std::map<std::string, std::vector<int>> class_onehot = {
         {"Iris-setosa", {1, 0, 0}},
         {"Iris-versicolor", {0, 1, 0}},
         {"Iris-virginica", {0, 0, 1}}
     };
 
-    int features = csv[0].size();
-    int rows = csv.size()-1;
+    int features = 4;
+    int rows = csv.size();
     int classes = 3;
 
-    Tensor df({rows, features-1});
+    Tensor df({rows, features});
     Tensor target({rows, classes});
-    for (int i = 1; i < csv.size(); ++i) {
-        for (int j = 1; j < features - 1; ++j) {
-            df[i-1][j-1] = std::stod(csv[i][j]); // ignore headers and id
+    for (int i = 0; i < csv.size(); ++i) {
+        for (int j = 1; j < features; ++j) {
+            df[i][j-1] = std::stod(csv[i][j]); // ignore id
         }
-        std::vector<int> onehot = class_onehot[csv[i][features-1]];
+        std::vector<int> onehot = class_onehot[csv[i][features+1]];
         for (int j = 0; j < onehot.size(); ++j) {
-            target[i-1][j] = onehot[j];
+            target[i][j] = onehot[j];
         }
     }
-    currentTime = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
-    std::cout << "df: " << std::ctime(&currentTime);
+
+    // split train test
+    int train = rows * 0.8, test = rows - train;
+    Tensor df_train({train, features});
+    Tensor target_train({train, classes});
+    Tensor df_test({test, features});
+    Tensor target_test({test, classes});
+
+    for (int i = 0; i < train; ++i) {
+        for (int j = 0; j < features; ++j) {
+            df_train[i][j] = df[i][j]->data;
+        }
+        for (int j = 0; j < classes; ++j) {
+            target_train[i][j] = target[i][j]->data;
+        }
+    }
+
+    for (int i = 0; i < test; ++i) {
+        for (int j = 0; j < features; ++j) {
+            df_test[i][j] = df[i + train][j]->data;
+        }
+        for (int j = 0; j < classes; ++j) {
+            target_test[i][j] = target[i + train][j]->data;
+        }
+    }
+
+    printf("train size: %i, test size: %i\n", df_train.shape[0], df_test.shape[0]);
 
     // model
-    Linear l1(5, 64);
-    Linear l2(64, 3);
-    Softmax s;
-    SGD optim({l1.params(), l2.params()}, 0.001);
+    Linear l1(4, 16);
+    Linear l2(16, 3);
+    SGD optim({l1.params(), l2.params()}, 0.01);
 
-    // learn
-
-    // --------- 1 epoch
-    // optim.zero_grad();
-    // Value_ptr loss = std::make_shared<Value>(0);
-    // // for (int j = 0; j < df.size(); ++j) {
-    // int j = 0;
-    // {
-    //     Tensor& input = df[j].first;
-    //     Tensor& target = df[j].second;
-    //     Tensor x = l1.forward(input).relu();
-    //     x = l2.forward(x).relu();
-    //     // x = s.forward(x);
-    //     loss = loss + MSELoss(x, target);
-    // }
-
-    // loss->backward();
-    // optim.step();
-    // printf("loss: %f\n", loss->data);
-    // for (int i = 0; i < l1.weights.total_count; ++i)
-    //     printf("(%f %f) ", l1.weights.values[i]->data, l1.weights.values[i]->grad);
-    // for (int i = 0; i < l1.biases.total_count; ++i)
-    //     printf("(%f %f) ", l1.biases.values[i]->data, l1.biases.values[i]->grad);
-    // printf("\n\n");
-    // for (int i = 0; i < l2.weights.total_count; ++i)
-    //     printf("(%f %f) ", l2.weights.values[i]->data, l2.weights.values[i]->grad);
-    // for (int i = 0; i < l2.biases.total_count; ++i)
-    //         printf("(%f %f) ", l2.biases.values[i]->data, l2.biases.values[i]->grad);
-    // printf("\n");
-
-
-    // ---------- epochs
-    int epochs = 100;
+    // train
+    printf("TRAIN\n");
+    int epochs = 300;
     for (int i = 0; i < epochs; ++i)
     {
-        currentTime = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
-        std::cout << "epoch " << i << " " << std::ctime(&currentTime);
         optim.zero_grad();
-        Tensor x = l1.forward(df).relu();
-        x = l2.forward(x).relu();
-        Value_ptr loss = MSELoss(x, target);
-        currentTime = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
-        std::cout << "forward: " << std::ctime(&currentTime);
-
+        Tensor x = l1.forward(df_train).relu();
+        x = l2.forward(x);
+        Value_ptr loss = MSELoss(x, target_train);
         loss->backward();
-
-        currentTime = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
-        std::cout << "backward: " << std::ctime(&currentTime);
         optim.step();
         printf("epoch: %i, loss: %f\n", i, loss->data);
-        // for (int i = 0; i < l2.weights.total_count; ++i)
-        //     printf("(%f %f) ", l2.weights.values[i]->data, l2.weights.values[i]->grad);
-        // printf("\n");
     }
 
+    // test
+    printf("TEST\n");
+    Tensor result = l1.forward(df_test).relu();
+    result = l2.forward(result);
+    result = result.argmax(1);
+
+    int correct = 0;
+    for (int i = 0; i < target_test.shape[0]; ++i)
+    {
+        int result_idx = result[i][0].get()->data, target_idx = -1;
+        printf("pred: %i, ", result_idx);
+        for (int j = 0; j < classes; ++j)
+        {
+            if (target_test[i][j].get()->data == 1.0) {
+                target_idx = j;
+                printf("target: %i", j);
+            }
+        }
+        if (target_idx == -1) printf("TARGET ERROR");
+        if (result_idx == -1) printf("RESULT ERROR");
+        printf("\n");
+        
+        correct += result_idx == target_idx;
+    }
+
+    printf("correct: %i\n", correct);
+    printf("correct% : %f\n", float(correct)/float(test));
 }
--- content.md
+++ content.md
@@ -6,3 +6,10 @@
 - argmax (2d with last dim)
 ## optimizers
 - sgd
+## examples
+- iris with ~90% accuracy
+
+## TODO
+- argmax full
+- cuda
+- move softmax, relu
