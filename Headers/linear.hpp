520f1b7805a91155e7d98c9a5b535bfb7bac7999
Pawel Kotzbach <pawkotz@gmail.com>
2026-02-02 22:45:15 +0100

    device to linear

--- Headers/linear.hpp
+++ Headers/linear.hpp
@@ -4,8 +4,11 @@
 #include "tensor.hpp"
 #include <stdexcept>
 
-struct Component {
-    virtual Tensor forward(Tensor input) {
+struct Component
+{
+    std::string device;
+    virtual Tensor forward(Tensor input)
+    {
         throw std::runtime_error("forward not implemented");
     }
 };
@@ -17,10 +20,12 @@ struct Linear : Component
     Tensor weights;
     Tensor biases;
 
-    Linear(int in_size, int out_size) : in_size(in_size), out_size(out_size), weights({out_size, in_size}), biases({out_size}) {}
+    Linear(int in_size, int out_size, std::string device = "cpu") : in_size(in_size),
+                                                                    out_size(out_size), weights({out_size, in_size}, false, device), 
+                                                                    biases({out_size}, false, device) { this->device = device; }
     ~Linear() {}
     Tensor forward(Tensor input) override;
-    std::vector<Tensor*> params() {return {&weights, &biases};}
+    std::vector<Tensor*> params() { return {&weights, &biases}; }
 };
 
 struct Softmax : Component
